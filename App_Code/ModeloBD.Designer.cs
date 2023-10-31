bd1384094f71ed7086ee58503f354df21626f367
AndresCastro <andrezkstro@gmail.com>
2023-10-30 21:06:24 -0500

    ok merge

--- App_Code/ModeloBD.Designer.cs
+++ App_Code/ModeloBD.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Cristian Barrantes\OneDrive - ut.edu.co\Escritorio\Pagina Web Dev\App_Code\ModeloBD.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\Proyectos\Eccargo\PSE2\App_Code\ModeloBD.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
